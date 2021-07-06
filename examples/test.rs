9e190bd131cf2848371fea73068fc1bb02fe5685
Rhys Holloway <rhyswilliamholloway@gmail.com>
2021-07-05 17:28:57 -0700

    Move to this repository

--- examples/test.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let start = std::time::Instant::now();
-    audio_builder::compile("music", "output/audio.bin")?;
-    println!("Finished in {}ms.", start.elapsed().as_millis());
-    Ok(())
-}
